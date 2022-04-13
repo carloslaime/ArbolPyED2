fc6efd058463a98f85d90b83cd79ddff00083b20
carloslaime <carlos.laime.99@gmail.com>
2022-04-13 16:19:30 -0400

    Implem class Node y Arbol + methods getter y setter

--- Arbol.py
+++ Arbol.py
@@ -1,2 +1,41 @@
+'''
+Titulo: Clase Arbol para desarrollar una estructura ADT Tree
+Nombre: Carlos Alberto Laime Navia
+Fecha: 13/04/22
+Version: v1.0
+'''
+from Nodo import Node
+'''Importación de la clase "Node" para ayudar a crear la
+clase Arbol'''
+#. Clase Arbol que representará la estructura ADT Tree
 class Arbol:
-  pass
+    ''' Metodo que crea la instancia de la clase Arbol'''
+    def __init__(self):
+        self.__raiz = None
+    ''' Argumentos de la clase Arbol
+	Nota: 
+		El atributo "raiz" es privado definido para la 
+		creación del Arbol y el Nodo origen del mismo
+	Argumentos:
+		"raiz": hace referencia a un Nodo que se tomará como 
+		punto de origen para la creación de otros nodos 
+		llamados también hojas.
+	Retorna:
+		"raiz": retornará la referencia del nodo.
+	'''  
+#. Metodos de la clase Arbol
+#. Metodos getter y setter de la clase Arbol
+    def getRaiz(self):
+        ''' 
+        Este método retorna la referencia en memoria de la 
+        instancia "raiz".
+        '''
+        return self.__raiz;
+
+    def setRaiz(self, x):
+        '''
+        Este método asignará la referencia de un nodo como 
+        punto de origen del objeto árbol.
+        '''
+        self.__raiz = x
+        
--- Nodo.py
+++ Nodo.py
@@ -1,20 +1,84 @@
 '''
-
-Titulo: Desarrollar la plantilla propuesta
-
-Implementar esta class mediante las directrices de style code de python y documentar siguiendo las buenas practicas planteadas en clases.
-
-Nota: Crear modulos para las clases Nodo y Arbol e implementar los metodos
-basicos de getter y setter de cada clase propuesta
+Titulo: Clase Nodo para desarrollar una estructura ADT Tree
+Autor: Carlos Alberto Laime Navia
+Fecha: 13/04/22
+Version: v1.0
 '''
-
+#. Clase Node que representará la estructura ADT Tree
 class Node:
-       def __init__(self, value):
-       self.left = None
-       self.right = None
-       self.data = value
-
+      ''' Metodo que crea la instancia de la clase Node''' 
+      def __init__(self, value):
+            self.__left = None
+            self.__right = None
+            self.__data = value
+      ''' Argumentos de la clase Node
+      Nota:
+            los atributos "left", "right" y "data" son tres
+            atributos privados definidos para la creación de
+            las hojas que conformarán el árbol.
+      Argumentos:
+            "left": hace referencia a otro nodo que se debe
+            encontrar a la izquierda de este.
+            "right": hace referencia a otro nodo que se debe
+            encontrar a la derecha de este.
+            "data": hace referencia al valor que almacenará 
+            el nodo actual.
+      Retorna:
+            "left": retornará la referencia del nodo izqdo.
+            "right": retornará la referencia del nodo derecho.
+            "data": retornará el valor que contenga el nodo 
+            actual.
+      '''
+#. Metodos de la clase Node
+#. Metodos getter y setter de la clase Node 
       def getData(self):
+            ''' 
+            Este métotodo devuelve el valor de un dato 
+            almacenado en la instancia de la clase Node.
+            '''
             return self.__data; 
+
       def setData(self, x):
+            ''' 
+            Este método settea el valor ingresado por 
+            parámetro mediante la variable "x" y lo 
+            almacena en el atributo "data" de la 
+            instancia creada.
+            '''
             self.__data = x
+      
+      def getLeft(self):
+            '''
+            Este métotodo devuelve el valor que contiene
+            el atributo "Left" en la instancia de la 
+            clase Node.
+            '''
+            return self.__left;
+      
+      def setLeft(self, x):
+            ''' 
+            Este método settea el valor ingresado por 
+            parámetro mediante la variable "x" y lo 
+            almacena en el atributo "Left" de la 
+            instancia creada.
+            '''
+            self.__left = x
+      
+      def getRight(self):
+            '''
+            Este métotodo devuelve el valor que contiene
+            el atributo "Right" en la instancia de la 
+            clase Node.
+            '''
+            return self.__right;
+      
+      def setRight(self, x):
+            ''' 
+            Este método settea el valor ingresado por 
+            parámetro mediante la variable "x" y lo 
+            almacena en el atributo "Right" de la 
+            instancia creada.
+            '''
+            self.__right = x
+
+      
